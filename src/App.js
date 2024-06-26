e568e581c74a149c423a74b34108f3f1a27f08c4
‘Lokesh <lokesh.cdewanand@gmail.com>
2024-06-26 18:16:05 +0530

    Package : stable versions

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import "./App.css";
 import EditorContainer from "./Container";
 
+import penIcon from "./Resources/pen.png";
+
 function App() {
 	return (
 		<div className="App">
+			<div className="documentTitle">
+				<div className="titleSection">
+					<img src={penIcon} width={10} height={10} alt="" />
+					Untitlted
+				</div>
+			</div>
 			<EditorContainer />
 		</div>
 	);
--- src/Container/index.jsx
+++ src/Container/index.jsx
@@ -12,7 +12,14 @@ export default function EditorContainer() {
 			setSourceCode(`
                 <html>
                     <head>
+						<link rel="preconnect" href="https://fonts.googleapis.com" />
+						<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+						<link
+							href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600&display=swap"
+							rel="stylesheet"
+						/>
                         <style>
+						
                             ${cssTags}
                         </style>
                     </head>
